4cb731082aa6aaa4e91046c8bdd40f79c5de80b1
Gaurav <gauravupreti232+github@gmail.com>
2025-09-06 17:22:05 +0530

    modules for app

---
+++ scripts/classification.py
@@ -0,0 +1,2 @@
+def ClassificationPipeline(data):
+    return data
---
+++ scripts/segmentation.py
@@ -0,0 +1,5 @@
+def SegmentationPipeline2D(flair , t1ce):
+    return data
+
+def SegmentationPipeline3D(flair, t1ce):
+    return data
